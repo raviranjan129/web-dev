858d9cd1ee7f6228d30f2c19ffe11526e5a213a7
name <email>
2023-12-18 19:14:17 +0530

    array

---
+++ array/slice.js
@@ -0,0 +1,10 @@
+ //  The slice() method of Array returns a copy of a portion of an array into a new array ;
+ // Return  a copy of a section of an array;
+
+ let arr = [10,20,30,40,50];
+  console.log(arr.slice(0,3)); // it returns a copy of array but exlude the last index; i.e [0,3);
+
+  console.log(arr.slice(-3,-1));  // it also return the negative index;
+
+  console.log(arr.slice(-2,-5));  //  it do not trace the index in reverse order that's why it returns empty array; i.e [];
+
